93042cbde44e62a6596bdda685ae81025aa14edd
tstyer <travisstyer.ts@gmail.com>
2025-09-21 14:36:28 +0100

    Ammend current models for learning

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
-    tite = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="blog_posts")
@@ -15,6 +15,12 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta():
+        ordering = ["-created_on"]
+    
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
@@ -25,4 +31,11 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+    
+    def _str_(self):
+        return f"Comment {self.body} by {self.author}"
+
 
