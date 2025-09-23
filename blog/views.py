6a33a8be2bd42502dfa9b401c665e8f974a643c5
tstyer <travisstyer.ts@gmail.com>
2025-09-23 11:41:02 +0100

    Add base.html

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,5 @@ from .models import Post
 # Create your views here.
 
 class PostList(generic.ListView):
-    model = Post
+    queryset = Post.objects.filter(author=1)
     
--- codestar/settings.py
+++ codestar/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
@@ -62,7 +64,7 @@ ROOT_URLCONF = 'codestar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
